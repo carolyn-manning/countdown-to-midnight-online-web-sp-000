192bd093d577e117fa882308762a8aaa277e0b29
Carolyn Manning <carolyn.manning3@gmail.com>
2020-09-21 23:54:17 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
-def countdown(integer)
+def countdown(number)
   while number > 0
-    puts "#{number} SECOND(S)!"
+    "#{number} SECOND(S)!"
     number -= 1
   end
-  return "HAPPY NEW YEAR!"
+ "HAPPY NEW YEAR!"
 end
